73d74569038f817d47f9a49e02ad04381f1d82cf
ankitasweetsprinkles01 <goddeterminestherighttime@gmail.com>
2022-07-11 00:06:32 +0530

    Ankita Chatterjee|A91005219157|Amity

--- script.js
+++ script.js
@@ -1,40 +1,31 @@
-// Player name
-var PLAYER1 = "PLAYER 1";
-var PLAYER2 = "PLAYER  2";
-
-// Function to change the player name
-function editNames() {
-    PLAYER1 = prompt("Change PLAYER1 name");
-    PLAYER2 = prompt("Change PLAYER2 name");
-
-    document.querySelector("p.PLAYER1").innerHTML = PLAYER1;
-    document.querySelector("p.PLAYER2").innerHTML = PLAYER2;
-}
-
-// Function to roll the dice
-function rollTheDice() {
-    setTimeout(function () {
-        var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-        var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-        document.querySelector(".img1").setAttribute("src",
-            "dice" + randomNumber1 + ".png");
-
-        document.querySelector(".img2").setAttribute("src",
-            "dice" + randomNumber2 + ".png");
-
-        if (randomNumber1 === randomNumber2) {
-            document.querySelector("h1").innerHTML = "Draw!";
-        }
-
-        else if (randomNumber1 < randomNumber2) {
-            document.querySelector("h1").innerHTML= (PLAYER2 + " WINS!");
-        }
-
-        else {
-            document.querySelector("h1").innerHTML
-                            = (PLAYER1 + " WINS!");
-                            
-        }
-    }, 2500);
+var target=20;
+var p1=0;
+var p2=0;
+var count=1;
+function play()
+{
+  if(count%2== 1)
+  {
+    var player1 = (Math.floor(Math.random() * 10)%6)+1 ;
+    document.getElementById("score").value = player1
+    p1 += player1;
+    document.getElementById("in1").value = p1;
+  }
+  else
+    {
+      var player2 = (Math.floor(Math.random() * 10)%6)+1 ;
+      document.getElementById("score").value = player2
+      p2 += player2;
+      document.getElementById("in2").value = p2;
+    }
+  count++;
+  if(p1 >= target)
+    {
+      document.getElementById("out").innerHTML = "Player 1 won the match";
+    }
+  else if(p2 >= target)
+    {
+      document.getElementById("out").innerHTML = "Player 2 won the match";
+  
+    }
 }
